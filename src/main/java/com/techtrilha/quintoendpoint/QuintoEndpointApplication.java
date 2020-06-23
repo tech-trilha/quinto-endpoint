e3b1d9ac9442645fecd36b0041381a995bd75669
Felipe Miranda <felip.simi@gmail.com>
2020-06-23 19:26:59 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/quintoendpoint/QuintoEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.quintoendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintoEndpointApplication.class, args);
+	}
+
+}
